2e9bb76b7c5eb323b237e808a1d97a908cefc0fc
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2025-03-21 13:57:41 -0700

    fixes routing and style errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    config: { path: './tailwind.config.mjs' },
+    applyBaseStyles: true
+  })],
   build: {
     format: 'file'
   },
   routing: {
     prefixDefaultLocale: false
-  }
+  },
+  // Make sure assets are correctly referenced in nested routes
+  base: '/',
+  trailingSlash: 'always'
 });
